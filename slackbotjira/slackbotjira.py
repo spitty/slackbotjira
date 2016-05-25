cfadcffe3a4303bdcb0c1cf3cdb9198f25c0b087
Maxim Logunov <spitty.pnz@gmail.com>
2016-05-25 23:44:50 +0300

    Find multiple tickets in messages

--- slackbotjira/slackbotjira.py
+++ slackbotjira/slackbotjira.py
@@ -1,6 +1,7 @@
 from jira import JIRA, JIRAError
 from slackbot import settings
 from slackbot.bot import respond_to, listen_to
+import re
 
 # Clean JIRA Url to not have trailing / if exists
 CLEAN_JIRA_URL = settings.JIRA_URL if not settings.JIRA_URL[-1:] == '/' else settings.JIRA_URL[:-1]
@@ -10,15 +11,18 @@ jira = JIRA(CLEAN_JIRA_URL, basic_auth=settings.JIRA_AUTH)
 
 
 # Listen for messages that look like JIRA URLs
-@respond_to('([A-Za-z]+)-([0-9]+)')
-@listen_to('([A-Za-z]+)-([0-9]+)')
-def jira_listener(message, ticketproject, ticketnum):
+@respond_to('(.*)')
+@listen_to('(.*)')
+def jira_listener(message, text):
   # Only attempt to find tickets in projects defined in slackbot_settings
-  if ticketproject not in settings.JIRA_PROJECTS:
-    return
+  for ticketproject, ticketnum in re.findall('([A-Za-z]+)-([0-9]+)', text):
+    if ticketproject not in settings.JIRA_PROJECTS:
+      continue
+    # Parse ticket and search JIRA
+    ticket = '%s-%s' % (ticketproject, ticketnum)
+    process_ticket(message, ticket)
 
-  # Parse ticket and search JIRA
-  ticket = '%s-%s' % (ticketproject, ticketnum)
+def process_ticket(message, ticket):
   try:
     issue = jira.issue(ticket)
   except JIRAError:
